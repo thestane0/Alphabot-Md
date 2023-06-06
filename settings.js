5bde92003137ac7c4e28b59a2381cf8062b4e028
TheDevil16 <135714467+TheDevil16@users.noreply.github.com>
2023-06-06 16:58:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,24 +27,24 @@ global.language = english  //change indonesia to english if you don't understand
 
 // Other
 // Other
-global.botname = "miko 𓆩☬𓆪" //namabot kalian
-global.ownername= "𝐋𝐔𝐂𝐈𝐅𝐄𝐑 𓆩☬𓆪" //nama kalian
+global.botname = "TheDevil 𓆩☬𓆪" //namabot kalian
+global.ownername= "TheDevil 𓆩☬𓆪" //nama kalian
 global.myweb ="https://chat.whatsapp.com/EcQ9l310H3A680NPXf3Hah" //bebas asal jan hapus
 global.youtube = "https://chat.whatsapp.com/EcQ9l310H3A680NPXf3Hah" //bebas asal jan hapus
 global.myweb2 = "https://chat.whatsapp.com/EcQ9l310H3A680NPXf3Hah" //bebas
 global.email = "mo685486@gmail.com" //bebas
 global.region = "Sri Lanka" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['201554861890',''] //premium user
-global.owner = ["201554861890",""] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "201554861890","" // nomor wa kalian
-global.ownernomerr = "+201554861890","" //nmr wa kalian
+global.premium = ['201554861890','01128060617'] //premium user
+global.owner = ["201554861890","01128060617"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "201554861890","01128060617" // nomor wa kalian
+global.ownernomerr = "+201554861890","01128060617" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '𝐋𝐔𝐂𝐈𝐅𝐄𝐑 𓆩☬𓆪' //sticker wm ubah
-global.author = '𝐋𝐔𝐂𝐈𝐅𝐄𝐑 𓆩☬𓆪' //sticker wm ganti nama kalian
+global.packname = 'TheDevil 𓆩☬𓆪' //sticker wm ubah
+global.author = 'TheDevil 𓆩☬𓆪' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
